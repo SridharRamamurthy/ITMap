4d2539d13bcfa862daec6bda2c25bf284e9602c5
sridharramamurthy <sridhar3011@gmail.com>
2022-08-18 09:46:28 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import 'font-awesome/css/font-awesome.min.css';
+import Login from './login';
+
+const HomeContent = () => {
+  return (
+    <div class="flex-container">
+    <div style={{ display: "flex", flexDirection: "row" }} >
+        <div className="" style={{
+            "backgroundImage": "linear-gradient(134deg, #b32e86 5%, #152e7a 91%)"
+        }} >
+            {
+               "IT Map"
+            }
+        </div>
+        <div className="col-10 pl-0 pr-0" style={{ backgroundColor: "#ffffff" }} >
+            <Login />
+        </div>
+    </div>
+</div>)
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HomeContent />
     </div>
   );
 }
